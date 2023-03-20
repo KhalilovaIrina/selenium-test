37c2f8f504e1fa9f78b90481519f7a276235dbc1
Khalilova <sharova-ira90@yandex.ru>
2023-03-20 18:18:50 +0300

    headless-on

--- src/test/java/HappyPath.java
+++ src/test/java/HappyPath.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 class HappyPath {
 
@@ -17,7 +18,11 @@ class HappyPath {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
